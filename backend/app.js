cd950e6abc3c5a5eb514d9b71dbab94618792fc0
utpal-kumar-08 <um697893@gmail.com>
2025-06-21 00:28:07 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -38,4 +38,10 @@ app.get("/", (req, res) => {
   res.send("API is running!");
 });
 app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 export default app;
